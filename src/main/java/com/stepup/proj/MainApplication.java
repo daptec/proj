e5f657d8fcb14f22a04608613a094c6d45758207
Andrey Chabanov <daptec@mail.ru>
2025-02-02 11:54:50 +0300

    Добавлен метод doSomething()

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,8 +3,12 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("1111");
-        System.out.println("2222");
-        System.out.println("3333");
+        System.out.println("1001");
+    }
+
+    public static void doSomething() {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
     }
 }
